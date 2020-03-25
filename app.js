ec3a6da1dc2b10cf976766580262d226a9de4f8a
Emmanuel Kamala <emmanuelkamala86@gmail.com>
2020-03-25 11:42:50 +0300

    Add input data function

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // BUDGET CONTROLLER
 
 let budgetController = (function(){
-
+   
 })();
 
 
@@ -9,17 +9,41 @@ let budgetController = (function(){
 
 let UIController = (function(){
 
+    let DOMstrings = {
+       inputType: '.add__type',
+       inputDescription: '.add__description',
+       inputValue: '.add__value',
+       inputBtn: '.add__btn'
+    };
+
+    return {
+        getInput: function() {
+            return {
+                type: document.querySelector(DOMstrings.inputType).value,
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value
+            };
+        },
+
+        getDOMstrings: function() {
+            return DOMstrings;
+        }
+    };
+
 })();
 
 // GLOBAL APP CONTROLLER
 
-let controller = (function(budgetCtrl,UICtrl){
+let controller = (function(budgetCtrl, UICtrl){
+
+    let DOM = UICtrl.getDOMstrings();
 
     let ctrlAddItem = function() {
-
+       let input = UICtrl.getInput();
+       console.log(input);
     }
 
-   document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+   document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
 
    document.addEventListener('keypress', function(event){
      if (event.keyCode === 13 || event.which === 13) {
