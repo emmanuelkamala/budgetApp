baaee6ea4bc0e2804acd7e9759f9d703586d02a2
emmanuelkamala <emmanuelkamala86@gmail.com>
2020-06-25 23:54:08 +0300

    Edit app files

--- src/app.js
+++ src/app.js
@@ -1,115 +1,18 @@
-// BUDGET CONTROLLER
-
-let budgetController = (function(){
-   
-    let Expense = function(id, value, description){
-            this.id = id;
-            this.value = value;
-            this.description = description;
+class UI {
+    constructor() {
+      this.budgetFeedback = document.querySelector(".budget-feedback");
+      this.expenseFeedback = document.querySelector(".expense-feedback");
+      this.budgetForm = document.getElementById("budget-form");
+      this.budgetInput = document.getElementById("budget-input");
+      this.budgetAmount = document.getElementById("budget-amount");
+      this.expenseAmount = document.getElementById("expense-amount");
+      this.balance = document.getElementById("balance");
+      this.balanceAmount = document.getElementById("balance-amount");
+      this.expenseForm = document.getElementById("expense-form");
+      this.expenseInput = document.getElementById("expense-input");
+      this.amountInput = document.getElementById("amount-input");
+      this.expenseList = document.getElementById("expense-list");
+      this.itemList = [];
+      this.itemID = 0;
     }
-
-    let Income = function(id, value, description){
-            this.id = id;
-            this.value = value;
-            this.description = description;
-    }
-
-    let data = {
-        allItems: {
-            exp: [],
-            inc: []
-        },
-
-        totals: {
-            exp: 0,
-            inc: 0
-        }
-    }
-
-    return {
-        addItem: function(type, desc, val) {
-            let newItem, ID;
-
-            if (data.allItems[type] > 0){
-                ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
-            } else {
-                ID = 0;
-            }
-
-            
-
-            if (type === 'exp') {
-                newItem = new Expense(ID, desc, val);
-            } else {
-                newItem = new Income(ID, desc, val);
-            }
-
-            data.allItems[type].push(newItem);
-            return newItem;
-        }
-    }
-})();
-
-
-// UI CONTROLLER
-
-let UIController = (function(){
-
-    let DOMstrings = {
-       inputType: '.add__type',
-       inputDescription: '.add__description',
-       inputValue: '.add__value',
-       inputBtn: '.add__btn'
-    };
-
-    return {
-        getInput: function() {
-            return {
-                type: document.querySelector(DOMstrings.inputType).value,
-                description: document.querySelector(DOMstrings.inputDescription).value,
-                value: document.querySelector(DOMstrings.inputValue).value
-            };
-        },
-
-        getDOMstrings: function() {
-            return DOMstrings;
-        }
-    };
-
-})();
-
-// GLOBAL APP CONTROLLER
-
-let controller = (function(budgetCtrl, UICtrl){
-
-    let setupEventListeners = function() {
-        let DOM = UICtrl.getDOMstrings();
-
-        document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
-
-        document.addEventListener('keypress', function(event){
-            if (event.keyCode === 13 || event.which === 13) {
-
-                ctrlAddItem();
-            }
-        });
-    };
-
-    let ctrlAddItem = function() {
-       let input, newItem;
-       
-       input = UICtrl.getInput();
-       
-       newItem = budgetCtrl.addItem(input.type, input.description, input.value);
-    }
-
-    return {
-        init: function(){
-            console.log('Application has started');
-            setupEventListeners();
-        }
-    };
-
-})(budgetController, UIController);
-
-controller.init();
+  }
